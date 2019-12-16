24b546e3bff055451c43026b6311bb01157c96bf
GitDVbootcamp <d_vasquez88@hotmail.com>
2019-12-16 01:45:38 -0700

    atm

---
+++ godaddyatm/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+
+function App() {
+  return (
+    <Form className="login-form">
+      <h1 className="text-center">Welcome to the GoDaddy ATM.</h1>
+      <br>
+      </br>
+      <br>
+      </br>
+      <br>
+      </br>
+      <h2 className="text-center">Enter your account number to get started.</h2>
+      <FormGroup>
+        <Input type="search" placeholder="Account Number"></Input>
+      </FormGroup>
+      <Button className="btn-lg btn-primary btn-block">Login</Button>
+    </Form>
+  );
+}
+
+export default App;
